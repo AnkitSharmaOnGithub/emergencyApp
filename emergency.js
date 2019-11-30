b9ecc430ddf34c66d9c7f10013c952a5ace6872d
Ankit Kumar Sharma <53289186+AnkitSharmaOnGithub@users.noreply.github.com>
2019-11-30 12:55:59 +0530

    Update emergency.js

--- emergency.js
+++ emergency.js
@@ -8,10 +8,17 @@
 //   Body: "And this is the body"
 // }).then(message => alert(message));
 
-Email.send({
-  SecureToken: "51b7e84a-2a65-4c25-ad28-a326dc097768",
-  To: "ankits@digitizebrand.com",
-  From: "email_testing@digitizebrand.com",
-  Subject: "This is the subject",
-  Body: "And this is the body"
-}).then(message => alert(message));
+// Email.send({
+//   SecureToken: "51b7e84a-2a65-4c25-ad28-a326dc097768",
+//   To: "ankits@digitizebrand.com",
+//   From: "email_testing@digitizebrand.com",
+//   Subject: "This is the subject",
+//   Body: "And this is the body"
+// }).then(message => alert(message));
+
+if (navigator.geolocation) {
+  console.log('Geolocation is supported!');
+}
+else {
+  console.log('Geolocation is not supported for this Browser/OS.');
+}
